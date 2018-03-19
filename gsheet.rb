7b3b5d9744694576487bdc0e6e0ed12324030f18
LetitiaShe <wehuin1215@gmail.com>
2018-03-19 20:53:03 +0800

    add README and spec

--- gsheet.rb
+++ gsheet.rb
@@ -17,61 +17,66 @@ module GsheetLib
       @sheet_service.create_spreadsheet(request_body)
     end
 
-    def copy_to(spreadsheet_id_from, spreadsheet_id_to)
+    def copy_to(spreadsheet_id_from, sheet_id, spreadsheet_id_to)
       request_body = Google::Apis::SheetsV4::CopySheetToAnotherSpreadsheetRequest.new
       request_body.destination_spreadsheet_id = spreadsheet_id_to
-      @sheet_service.copy_spreadsheet(spreadsheet_id_from, 0, request_body)
+      @sheet_service.copy_spreadsheet(spreadsheet_id_from, sheet_id, request_body)
     end
 
     def sheets(spreadsheet_id)
       response = @sheet_service.get_spreadsheet(spreadsheet_id)
-      puts response.sheets.to_json
+      response.sheets
     end
 
     def read(spreadsheet_id, range)
       response = @sheet_service.get_spreadsheet_values(spreadsheet_id, range)
       puts 'No data found.' if response.values.empty?
-      puts response.values.to_json
+      response.values
     end
 
-    def list
+    def list_all_spreadsheet
       files = @drive_service.fetch_all(items: :files) do |page_token|
-        @drive_service.list_files(q: "mimeType='application/vnd.google-apps.spreadsheet'",
-                                 spaces: 'drive',
-                                 fields: 'nextPageToken, files(id, name)',
-                                 page_token: page_token)
+        @drive_service.list_files(q: "mimeType = 'application/vnd.google-apps.spreadsheet' and trashed = false",
+                                  spaces: 'drive',
+                                  fields: 'nextPageToken, files(id, name)',
+                                  page_token: page_token)
       end
-      files.each do |file|
-        # Process change
-        puts "Found file: #{file.name} #{file.id}"
-      end
+      # files.each_with_index do |file, i|
+      #   # Process change
+      #   puts "#{i}. Found file: #{file.name} #{file.id}"
+      # end
+      files.map { |file| file }
     end
 
-    def delete_sheet(spreadsheet_id)
+    def delete_spreadsheet(spreadsheet_id)
       @drive_service.delete_file(spreadsheet_id)
     end
 
     def add_member(spreadsheet_id, user_email)
+      permission = 0
+
       callback = lambda do |res, err|
         if err
           # Handle error...
           puts err.body
         else
-          puts "Permission ID: #{res.id}"
+          # puts "Permission ID: #{res.id}"
+          permission = res
         end
       end
 
       @drive_service.batch do |service|
         user_permission = {
-            type: 'user',
-            role: 'writer',
-            email_address: user_email
+          type: 'user',
+          role: 'writer',
+          email_address: user_email
         }
         service.create_permission(spreadsheet_id,
                                   user_permission,
                                   fields: 'id',
                                   &callback)
       end
+      permission
     end
   end
 end
--- spec.rb
+++ spec.rb
@@ -4,11 +4,59 @@ require 'yaml'
 require_relative 'sheet_opt.rb'
 
 describe 'Test Gsheet Library' do
-  APPLICATION_NAME = 'Google Sheets API Ruby Quickstart'.freeze
+  APPLICATION_NAME = 'GSpreadsheet Library'.freeze
   CLIENT_SECRETS_PATH = 'client_secret.json'.freeze
   SCOPE = ['https://www.googleapis.com/auth/spreadsheets',
            'https://www.googleapis.com/auth/drive.file'].freeze
-  describe 'Get Google Authorization' do
-    spreadsheet = GsheetLib::GoogleAPI.new(APPLICATION_NAME, CLIENT_SECRETS_PATH, SCOPE).spreadsheet
+  SPREADSHEET_EXIST = '1ItCeIVWgVRanDJ3vaZ7gd7ByuKC69Y5U8WLI-VP0sSA'.freeze
+  SHEET_ID = 0
+  RANGE = 'sheet1'.freeze
+  USER_EMAIL = 'wehuin1215@gmail.com'
+
+  describe 'Oprations on Google Spreadsheet' do
+    before do
+      @spreadsheet = GsheetLib::GoogleAPI.new(APPLICATION_NAME, CLIENT_SECRETS_PATH, SCOPE).spreadsheet
+    end
+
+    it 'Copy a exist sheet of one spreadsheet to another' do
+      new_spreadsheet = @spreadsheet.create
+      copy_sheet = @spreadsheet.copy_to(SPREADSHEET_EXIST, SHEET_ID, new_spreadsheet.spreadsheet_id)
+
+      _(copy_sheet.index).must_equal 1
+    end
+
+    it 'Read all the sheets documents from one exist spreadsheet' do
+      sheets = @spreadsheet.sheets(SPREADSHEET_EXIST)
+
+      _(sheets.length).must_equal 5
+    end
+
+    it 'Read the content of an exist sheet' do
+      read_sheet = @spreadsheet.read(SPREADSHEET_EXIST, RANGE)
+
+      _(read_sheet.length).must_equal 11
+    end
+
+    it 'List all the spreadsheet we have' do
+      new_spreadsheet = @spreadsheet.create
+      list = @spreadsheet.list_all_spreadsheet
+      files_id = list.map(&:id)
+
+      _(files_id).must_include new_spreadsheet.spreadsheet_id
+    end
+
+    it 'Add a co-author to an exist spreadsheet' do
+      new_spreadsheet = @spreadsheet.create
+      permission = @spreadsheet.add_member(new_spreadsheet.spreadsheet_id, USER_EMAIL)
+
+      _(permission).must_be_instance_of Google::Apis::DriveV3::Permission
+    end
+
+    it 'Delete an exist spreadsheet' do
+      new_spreadsheet = @spreadsheet.create
+      response = @spreadsheet.delete_spreadsheet(new_spreadsheet.spreadsheet_id)
+
+      _(response).must_be_empty
+    end
   end
 end
